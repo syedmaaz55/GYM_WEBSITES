4b6f59314aaff9e9b73a63fb5f78b8ea51ed498e
Syed Maaz <syedmaaz5532@gmail.com>
2026-02-21 22:52:05 +0500

    remove scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,11 +100,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 hidden md:flex flex-col items-center gap-2 text-zinc-500">
-          <span className="text-[10px] uppercase font-bold tracking-widest">Scroll</span>
-          <div className="w-[2px] h-12 bg-gradient-to-b from-[#73BB1B] to-transparent"></div>
-        </div>
+
       </section>
 
       {/* --- FEATURES GRID --- */}
